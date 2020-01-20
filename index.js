6c772d0fdd981963dde73b8324286dfad1e00a5e
Elinda Sintaresmi <elinda.sinta@gmail.com>
2020-01-20 15:13:50 +0700

    add feature edit

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 
+
+const listTodos = document.getElementsByClassName("todos")
+
 function addTask() {
     let input = document.getElementById("todo").value;
     let node = document.createElement("li");
     let textnode = document.createTextNode(input);
+    node.setAttribute("class", "todos")
+    node.setAttribute("ondblclick", "edit(event)")
+    node.setAttribute("id", `todo-${listTodos.length}`)
     node.appendChild(textnode);
     document.getElementById("newList").appendChild(node);
 
@@ -19,4 +25,27 @@ function addTask() {
         false
     );
     node.appendChild(removeTask);
+}
+
+function edit(event) {
+
+    const id = event.target.id
+    const text = prompt("Edit todo:")
+    // console.log(id);
+
+    const editTodo = document.getElementById(id)
+    editTodo.innerText = text
+
+    let removeTask = document.createElement("input");
+    removeTask.setAttribute("type", "button");
+    removeTask.setAttribute("value", "Remove");
+    removeTask.setAttribute("id", "removeButton");
+    removeTask.addEventListener(
+        "click",
+        function (e) {
+            editTodo.parentNode.removeChild(editTodo);
+        },
+        false
+    );
+    editTodo.appendChild(removeTask)
 }
